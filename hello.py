eb5e33c1fe864cb80c4d919bd7807e118af82ba1
sulynajimsj <sulynajim2002@gmail.com>
2024-09-26 11:17:40 -0400

    Implemented form with email validation for Activity 1.4

--- hello.py
+++ hello.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+
+        return redirect(url_for('index'))
     return render_template('index.html', 
-                           current_time=datetime.utcnow(),
-                           name='Suleiman')
+                           form=form, name=session.get('name'),
+                           email=session.get('email'),
+                           current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True)
